b2d0087e178e65b4ffcb1ed2ed37fb88f9b4ccc9
VitMach <machihin_vitalii@mail.ru>
2024-10-09 23:27:22 +0300

    Fix

--- src/shared/api/quotes/quotes.ts
+++ src/shared/api/quotes/quotes.ts
@@ -1,8 +1,6 @@
 import { apiClient } from "../base";
 import { QuoteType } from "./types";
 
-const BASE_URL = "/?method=getQuote&key=random&format=json&lang=ru";
-
 export const getQuotes = (): Promise<QuoteType> => {
 	return apiClient.get();
 };
