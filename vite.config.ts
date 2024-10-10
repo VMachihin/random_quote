04ac6ac18be7aab15bc4547a29594da2206ec998
VitMach <machihin_vitalii@mail.ru>
2024-10-10 23:57:58 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	server: {
+		proxy: {
+			"/api": {
+				target:
+					"https://thingproxy.freeboard.io/fetch/https://api.forismatic.com/api/1.0/",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
 });
